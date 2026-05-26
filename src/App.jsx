94e73f0e22d6e7e6e0d8462dcfd7e7886629b5ed
thummisettyvenkatesh <venkatesh@realtekconsulting.net>
2026-05-26 17:54:35 +0530

    Added cards, calendar, filter UI updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import JobListView from './components/JobListView';
 import CandidatesPage from './pages/CandidatesPage';
 import AddCandidatePage from './pages/AddCandidatePage';
 import DashboardPage from './pages/DashboardPage';
+import DemoSamplePage from './pages/DemoSamplePage';
 import UserForm from './components/form/forms';
 
 export default function App() {
@@ -30,6 +31,7 @@ export default function App() {
             }
           />
           <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/demosample" element={<DemoSamplePage />} />
           <Route path="/candidates" element={<CandidatesPage />} />
           <Route path="/candidates/add" element={<AddCandidatePage />} />
           <Route path="/add" element={<UserForm />} />
